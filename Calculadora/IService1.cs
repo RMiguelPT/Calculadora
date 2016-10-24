9682303213adfa28863e70508ca0064a46b3475d
DESKTOP-JCTKE6B\Ruben <ruben.miguel.55@gmail.com>
2016-10-24 21:10:21 +0100

    Add project files.

---
+++ Calculadora/IService1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Calculadora
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface iServicoCalculadora
+    {
+
+        [OperationContract]
+        int sum(int a, int b);
+        [OperationContract]
+        int subtraction(int a, int b);
+        [OperationContract]
+        int multiplication(int a, int b);
+        [OperationContract]
+        int divide(int a, int b);
+
+         // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+   
+}
---
+++ Calculadora/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace Calculadora
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ServicoCalculadora : iServicoCalculadora
+    {
+        public int divide(int a, int b)
+        {
+           if (b != 0)
+            {
+                return a / b;
+            }
+            return 0;
+        }
+
+        public int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        int iServicoCalculadora.sum(int a, int b)
+        {
+            return a + b;
+        }        
+
+    }
+}
